085d591073f4335818e03892b3657675dfeda26a
Iskander918 <iskan.sitdikov@gmail.com>
2025-10-20 04:19:23 +0300

    Edit main

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 int main (int argc, char* argv[]) {
 
-  std::cout << "Yo, test proj" << std::endl;
+  std::cout << "Yoooo, test proj" << std::endl;
 
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
